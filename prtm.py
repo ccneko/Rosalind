8ae6dd15fc32f00edb46fc0064826552b1bbb631
ccneko <ccneko@users.noreply.github.com>
2015-11-16 14:41:11 +0800

    Create prtm.py

---
+++ prtm.py
@@ -0,0 +1,18 @@
+#!/usr/env
+from util import *
+#calculate protein mass
+
+def protmass(s):
+	table = readaamass()
+	water = table[0]
+	aa = table[1]
+	aamass = table[2]
+
+	mass = 0
+	for i in s:
+		mass = mass + aamass[aa.index(i)]
+	print '%.3f' % mass
+	writeResult('prtm',str('%.3f' % mass))
+
+s=readData('rosalind_prtm.txt')[0].rstrip()
+protmass(s)
