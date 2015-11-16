6e5fb344f321294a9bc28fb8fa15937b3d2bc8e5
ccneko <ccneko@users.noreply.github.com>
2015-11-16 14:38:15 +0800

    Create iprb.py

---
+++ iprb.py
@@ -0,0 +1,22 @@
+#!/usr/env
+from util import *
+#mendelian inheritance
+
+def Pdom(k,m,n):
+	prob = 1.0
+	total = k + m + n
+	k1 = k/total
+	m1 = m/total
+	n1 = n/total
+	#prob = 1 - n1*(n1+m1/2) - m1*m1/4
+	#prob = 1 - 2 * n/total * (n-1)/(total-1) - 2* n/total * (m/2)/total - 2* (m/2)/total * ((m-1)/2)/(total - 1)
+	print '%.5f' % prob
+	writeResult('iprb', '%.5f' % prob)
+	return prob
+
+f = readData('rosalind_iprb.txt')[0].rsplit()
+k = float(f[0])
+m = float(f[1])
+n = float(f[2])
+
+Pdom(k,m,n)
