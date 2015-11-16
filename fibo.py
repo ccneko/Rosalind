94d59fb210b15eeab983c8cc6a31efe8d7252760
ccneko <ccneko@users.noreply.github.com>
2015-11-16 14:36:59 +0800

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,15 @@
+#!/usr/env
+from util import *
+#fibonacci
+
+def fibo(n):
+	a1 = 1
+	a2 = 1
+	for i in range(n-2):
+		temp = a2
+		a2 = a1 + a2
+		a1 = temp
+	print a2
+	writeResult('fibo',str(a2))
+  n=int(readData('rosalind_fibo.txt')[0].rsplit()[0])
+fibo(n)
