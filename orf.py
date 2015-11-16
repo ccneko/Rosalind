d38ea0acc780ea6b5f446776ec5498f1e396b396
ccneko <ccneko@users.noreply.github.com>
2015-11-16 14:40:20 +0800

    Create orf.py

---
+++ orf.py
@@ -0,0 +1,23 @@
+#!/usr/env
+from util import *
+#translate DNA to protein from ORF
+
+def translateORF(s):
+	protlist = translateORF_f(revc(s),translateORF_f(s,[]))
+	for prot in protlist:
+		print prot
+		writeResult('orf',prot+'\n')
+
+	return protlist
+
+def translateORF_f(s,protlist):
+        for i in range(len(s)):
+                if s[i:i+3] == 'ATG':
+			prot =  translateDNA(s[i:])
+			if protlist is not None:
+				if prot is not None and prot not in protlist:
+					protlist.append(prot)
+	return protlist			
+
+s=readFASTA('rosalind_orf.txt')[1][0].rstrip()
+translateORF(s)
